c3904546c9d315d18888dc4197ea4a05c8ea127e
tp4129 <tp4129@edu.teicrete.gr>
2019-11-17 23:02:06 +0200

    V1.0.0.5

--- ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Main.java
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Main.java
@@ -1,41 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Project for TEI OF CRETE lesson
+ *  Plan Driven and Agile Programming  * 
+ *  TP4129 - TP4187 - TP4145  * 
  */
 package advance_java_team_clinic_project;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  *
  * @author Beast
  */
+
 public class Main {
-    
+
     public static void main(String[] args) {
-        Main pin = new Main();
+        DBConnection object = DBConnection.getInstance();
     }
-        public Main(){
-            
-        
-        
-            try (Connection conn = DriverManager.getConnection(
-                "jdbc:oracle:thin:@25.57.202.173:1521:orcl", "javadev", "javadev")) {
 
-            if (conn != null) {
-                System.out.println("Connected to the database!");
-            } else {
-                System.out.println("Failed to make connection!");
-            }
-
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
             
     }
-}
+
