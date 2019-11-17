dd0504539e033b0eeffb57edf22d79bb27acfe6c
Tasos Koumarelis <tp4187@edu.hmu.gr>
2019-11-17 19:45:47 +0200

    Tasos
    tasos at 20

--- ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/ADVANCE_JAVA_TEAM_CLINIC_PROJECT.java
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/ADVANCE_JAVA_TEAM_CLINIC_PROJECT.java
@@ -17,6 +17,7 @@ public class ADVANCE_JAVA_TEAM_CLINIC_PROJECT {
     public static void main(String[] args) {
         // TODO code application logic here
         // test123
+        //tasos
     }
     
 }
