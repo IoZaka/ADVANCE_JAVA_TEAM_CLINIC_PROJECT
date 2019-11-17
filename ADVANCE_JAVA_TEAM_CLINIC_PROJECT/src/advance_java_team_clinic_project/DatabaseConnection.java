26ab14b419c54a61275320caf9e7c75bd54f76dd
tp4129 <tp4129@edu.teicrete.gr>
2019-11-17 23:42:24 +0200

    V1.0.0.6

---
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/DatabaseConnection.java
@@ -0,0 +1,48 @@
+/*
+ *  Project for TEI OF CRETE lesson
+ *  Plan Driven and Agile Programming
+ *  TP4129 - TP4187 - TP4145
+ */
+package advance_java_team_clinic_project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Beast
+ */
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    Connection connection;
+    private String url =  "jdbc:oracle:thin:@localhost:1521:xe";
+    private String username = "javadev";
+    private String password = "javadev";
+    
+    private DatabaseConnection() throws SQLException {
+        try {
+            this.connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException ex){
+                System.out.println("Database Connection Creation Failed: " + ex.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()){
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+    
+
+   
+}
--- ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Main.java
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Main.java
@@ -1,9 +1,13 @@
 /*
  *  Project for TEI OF CRETE lesson
- *  Plan Driven and Agile Programming  * 
- *  TP4129 - TP4187 - TP4145  * 
+ *  Plan Driven and Agile Programming
+ *  TP4129 - TP4187 - TP4145
  */
 package advance_java_team_clinic_project;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -12,8 +16,25 @@ package advance_java_team_clinic_project;
 
 public class Main {
 
-    public static void main(String[] args) {
-        DBConnection object = DBConnection.getInstance();
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection object = DatabaseConnection.getInstance();
+        Statement stmt = object.connection.createStatement();
+        String sql = "select a.username as \"user\",a.password pass,b.description role,c.name||' ' || c.surname created_by,d.name||' ' || d.surname updated_by from pm_users a, pm_roles b, pm_users c, pm_users d where a.created_by = c.id and a.updated_by = d.id and a.role_id = b.id";
+        ResultSet rs = stmt.executeQuery(sql);
+        while(rs.next()){
+        String username = rs.getString("user");
+        String password = rs.getString("pass");
+        String role = rs.getString("role");
+        String created = rs.getString("created_by");
+        String updated = rs.getString("updated_by");
+        
+        System.out.println("User: " + username );
+        System.out.println("Password: "+ password);   
+        System.out.println("Role: " + role);
+        System.out.println("Created: " + created);
+        System.out.println("Updated: " + updated);
+        }
+        rs.close();
     }
 
             
