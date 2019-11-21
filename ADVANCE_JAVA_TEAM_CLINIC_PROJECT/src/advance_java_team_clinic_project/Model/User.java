e5739e116fea5899492e2419c41acf94e57624cd
tp4145 <tp4145@edu.hmu.gr>
2019-11-21 22:30:13 +0200

    1.0.1.3

--- ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Model/User.java
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Model/User.java
@@ -9,7 +9,7 @@ public class User {
     private String surname;
     private int addressID;
     private int contactID;
-
+    
     public int getId() {return id;}
     public void setId(int id) {this.id = id;}
 
