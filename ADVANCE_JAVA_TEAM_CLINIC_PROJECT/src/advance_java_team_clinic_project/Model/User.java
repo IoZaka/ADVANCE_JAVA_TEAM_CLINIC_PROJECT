24d2e7af03fe48456434c2c67a89e2aca85efbc5
tp4145 <tp4145@edu.hmu.gr>
2019-11-21 21:52:14 +0200

    1.0.1.0

--- ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Model/User.java
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Model/User.java
@@ -9,6 +9,28 @@ public class User {
     private String surname;
     private int addressID;
     private int contactID;
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getSurname() {return surname;}
+    public void setSurname(String surname) {this.surname = surname;}
+
+    public int getAddressID() {return addressID;}
+    public void setAddressID(int addressID) {this.addressID = addressID;}
+
+    public int getContactID() {return contactID;}
+    public void setContactID(int contactID) {this.contactID = contactID;}
+    
     
     
 }
