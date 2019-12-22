99f347622fa13137b4432b3f05cfd56383379793
Tasos Koumarelis <tp4187@edu.hmu.gr>
2019-12-22 21:06:15 +0200

    l

--- ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Model/DatabaseConnection.java
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Model/DatabaseConnection.java
@@ -17,7 +17,7 @@ public class DatabaseConnection {
     public static DatabaseConnection instance;
     public Connection connection;
     private String url =  "jdbc:oracle:thin:@localhost:1521:xe";
-    //private String url =  "jdbc:oracle:thin:@25.57.202.173:1521:xe";
+   // private String url =  "jdbc:oracle:thin:@25.57.202.173:1521:xe";
     private String username = "javadev";
     private String password = "javadev";
     
