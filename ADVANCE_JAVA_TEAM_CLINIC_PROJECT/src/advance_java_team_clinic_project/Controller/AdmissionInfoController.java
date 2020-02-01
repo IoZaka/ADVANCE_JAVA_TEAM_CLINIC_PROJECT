dde930f5f823117a70f026ab8501d17909db17b1
tp4145 <tp4145@edu.hmu.gr>
2020-02-01 23:42:13 +0200

    1.1.0.6

--- ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Controller/AdmissionInfoController.java
+++ ADVANCE_JAVA_TEAM_CLINIC_PROJECT/src/advance_java_team_clinic_project/Controller/AdmissionInfoController.java
@@ -6,6 +6,8 @@
 package advance_java_team_clinic_project.Controller;
 
 import advance_java_team_clinic_project.Model.Admission;
+import advance_java_team_clinic_project.Model.LoggedInUser;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,10 +21,15 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
@@ -66,6 +73,12 @@ public class AdmissionInfoController implements Initializable {
 
     private Admission admissionModel = new Admission();
     private ResultSet rs;
+    @FXML
+    private Button backBtn;
+    @FXML
+    private Button createBtn;
+    @FXML
+    private Button updateBtn;
     
     /**
      * Initializes the controller class.
@@ -74,7 +87,8 @@ public class AdmissionInfoController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        createBtn.setVisible(false);
+        updateBtn.setVisible(false);
     }
 
     /**
@@ -83,6 +97,19 @@ public class AdmissionInfoController implements Initializable {
      * @param ID
      */
     public void setAdmissionID(Integer diagID, Integer ID) {
+        
+        LoggedInUser loggedInUser = LoggedInUser.getInstance();
+        
+        //checks if the user is not a patient
+        if(loggedInUser.getRoleID() != 3){
+           //if it doesn't exist, show createBtn - if not show updateBtn
+            if(ID == -1){
+                createBtn.setVisible(true);
+            }else{
+                updateBtn.setVisible(true);
+            } 
+        }
+        
         totalCostInput.setEditable(false);
         rs = admissionModel.fetchAdmissionData(ID);
         try {
@@ -94,8 +121,7 @@ public class AdmissionInfoController implements Initializable {
                 updatedByInput.setText(rs.getString("updated_by"));
                 doctorsName.setText(rs.getString("doctor"));
                 patientsName.setText(rs.getString("patient"));
-                //System.out.println(rs.getString("discharge_date"));
-                System.out.println(rs.getString("admission_date"));
+                totalCostInput.setText(rs.getString("total_cost"));
                 admissionDateInput.setValue(LOCAL_DATE(rs.getString("admission_date")));
                 if(!rs.getString("discharge_date").equals("-1")){
                     dischargeDateInput.setValue(LOCAL_DATE(rs.getString("discharge_date")));
@@ -106,6 +132,23 @@ public class AdmissionInfoController implements Initializable {
         } catch (SQLException ex) {
             Logger.getLogger(AdmissionInfoController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+//        backBtn.setOnMouseClicked(new EventHandler<MouseEvent>(){
+//            @Override
+//            public void handle(MouseEvent event) {
+//                try {
+//                    FXMLLoader loader = new FXMLLoader(AdmissionInfoController.this.getClass().getResource("../View/DiagnosisInfoView.fxml"));
+//                    Parent root = (Parent) loader.load();
+//                    DiagnosisInfoController diagnosisID = loader.getController();
+//                    diagnosisID.setDiagnosisID(appID, diagID);
+//                    admissionInfoPane.getChildren().clear();
+//                    admissionInfoPane.getChildren().add(root);
+//                } catch (IOException ex) {
+//                    Logger.getLogger(Id_RecordViewController.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//            }
+//        });
+        
     }
     
      public static final LocalDate LOCAL_DATE(String dateString) {
